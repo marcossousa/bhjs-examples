a12ee3c55f727d1d3838a982cb649ccfbba6208c
Marcos Sousa <marcos.sousa@corp.globo.com>
2012-01-17 01:35:50 -0200

    Adding namespace to capitilize plugin

--- jquery-capitalize-1.0.0.js
+++ jquery-capitalize-1.0.0.js
@@ -5,25 +5,48 @@
 * Version: 1.0.0
 * Release: 2012-01-12
 */
+; (function($, document, window, undefined) {
+    
+    var methods = { 
+        init : function(options) {
+            
+            var defaults = {
+                forceLowerCase: false
+            };
 
- (function($) {
-    $.fn.capitalize = function(options) {
-
-        var defaults = {
-            forceLowerCase: false
-        };
-
-        var options = $.extend(defaults, options);
-
-        return this.each(function() {
-            var obj = $(this);
-            var text = obj.text();
-            if (options.forceLowerCase) {
-                text = text.toLowerCase();
-            }
-            text = text.charAt(0).toUpperCase() + text.slice(1);
-            obj.text(text);
-        });
+            var options = $.extend(defaults, options);
+            
+            return this.each(function() {
+                var obj = $(this);
+                var text = obj.text();
+                
+                obj.data('capitalize', text);
+                
+                if (options.forceLowerCase) {
+                    text = text.toLowerCase();
+                }
+                text = text.charAt(0).toUpperCase() + text.slice(1);
+                obj.text(text);
+            });
+        },
+        restore : function() {
+            return this.each(function() {
+                var obj = $(this);
+                var text = obj.data('capitalize');
+                obj.text(text);
+            });
+        }
+    }
+    
+    $.fn.capitalize = function(method) {
+        if(methods[method]) {
+            options = Array.prototype.slice.call(arguments, 1);
+            return methods[method].apply(this, options);
+        } else if ( typeof method == "object") {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error("The method" + method + " doens't exist");
+        }
     };
 
 })(jQuery);
